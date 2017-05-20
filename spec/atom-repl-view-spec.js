7df3062be7f0c61f072f134e1c96cb7b45cee280
Jason Knight <jolierouge242@gmail.com>
2017-05-20 07:29:51 +0200

    Initial Commit

---
+++ spec/atom-repl-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AtomReplView from '../lib/atom-repl-view';
+
+describe('AtomReplView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
